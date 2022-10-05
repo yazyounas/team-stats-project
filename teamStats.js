21610523b72c00223ceb5b9e0bb3585b0554d7c1
Yasir Younas <yazyounas@gmail.com>
2022-10-05 13:55:08 +0100

    added comments

--- teamStats.js
+++ teamStats.js
@@ -1,10 +1,15 @@
+
+//Team Stats Project Adding Players
+
 const team = {
+    // Players object with Players name
     _players : [
       {firstName: 'Roger', lastName: 'Manole', age: 50},
       {firstName: 'Yasir', lastName: 'Younas', age: 35},
       {firstName: 'Qasim', lastName: 'Khan', age: 29}
   
     ],
+    //games objects with games names
     _games : [
       {opponent: 'combat', teamPoints: 20, opponentPoints: 25},
       {opponent: 'chimo', teamPoints: 42, opponentPoints: 29},
@@ -16,6 +21,8 @@ const team = {
     get games () {
       return this._games;
     },
+
+    //added new player
     addPlayer(newFirstName, newLastName, newAge){
       let player = {
         FirstName : newFirstName,
@@ -24,6 +31,7 @@ const team = {
       }
       this.players.push(player);
     },
+    //added new game
     addGame(newOpponent, newTeamPoints, newOpponentPoints){
       let game = {
         opponent: newOpponent,
@@ -33,6 +41,7 @@ const team = {
       this.games.push(game);
     }
   };
+  //project validation
   team.addGame('Titans', 100, 98)
   console.log(team.games)
   team.addPlayer('bugs', 'Bunny',76)
